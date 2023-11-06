5c381e573cb10c5faf6daa4bddfb1d43989a8e77
jameel914 <jameelibrahim107@gmail.com>
2023-11-06 17:08:42 -0500

    setup controller

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const cityController = require("./controllers/cityController.js");
+
 app.use(cors());
 app.use(express.json());
+app.use("/cities", cityController);
 
 app.get("/", (req, res) => {
   res.status(200).send("Welcome to the popular cities in Europe!! ");
---
+++ controllers/cityController.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const cities = express.Router();
+
+module.exports = cities;
