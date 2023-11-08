24b1904b6296395439f7a4e8d6ea52b4bf5d296f
jameel914 <jameelibrahim107@gmail.com>
2023-11-07 23:25:44 -0500

    fixed image url

--- db/seed.sql
+++ db/seed.sql
@@ -1,9 +1,9 @@
 \c cities_dev;
 
 INSERT INTO cities (name, image, population, area, annual_visitors, currency, is_capital) VALUES 
-('London', 'https://www.pexels.com/photo/group-of-people-near-tower-bridge-1427579/', 9, 607, 19.6, 'Pound Sterling', true),
-('Paris', 'https://www.pexels.com/photo/architecture-blue-sky-city-cityscape-532826/', 2, 40.54, 19.1, 'Euro', true),
-('Istanbul', 'https://www.pexels.com/photo/brown-concrete-dome-building-at-night-1549326/', 16, 2109, 14.7, 'Turkish Lira', false),
-('Rome', 'https://www.pexels.com/photo/colosseum-italy-1797158/', 3, 496, 10.3, 'Euro', true),
-('Amsterdam', 'https://www.pexels.com/photo/boat-on-the-water-near-buildings-967292/', 1, 84.68, 8.8, 'Euro', true),
-('Barcelona', 'https://www.pexels.com/photo/aerial-photography-of-city-1388030/', 2, 39.34, 7, 'Euro', false);
+('London', 'https://images.pexels.com/photos/726484/pexels-photo-726484.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 9, 607, 19.6, 'Pound Sterling', true),
+('Paris', 'https://images.pexels.com/photos/161853/eiffel-tower-paris-france-tower-161853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 2, 40.54, 19.1, 'Euro', true),
+('Istanbul', 'https://images.pexels.com/photos/1549326/pexels-photo-1549326.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 16, 2109, 14.7, 'Turkish Lira', false),
+('Rome', 'https://images.pexels.com/photos/1797158/pexels-photo-1797158.jpeg', 3, 496, 10.3, 'Euro', true),
+('Amsterdam', 'https://images.pexels.com/photos/415352/pexels-photo-415352.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 1, 84.68, 8.8, 'Euro', true),
+('Barcelona', 'https://images.pexels.com/photos/1388030/pexels-photo-1388030.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 2, 39.34, 7, 'Euro', false);
